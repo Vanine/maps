6c53ab4dfb8fbfa256c9c10f9c22cf004f4dfc3c
Vanine Ghazaryan <vanine.ghazaryan01@gmail.com>
2020-01-08 23:58:01 +0400

    Added heroku configurations

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Cors = require('cors');
+const path = require('path');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(Cors());
@@ -19,7 +20,11 @@ mongoose.connect('mongodb://localhost:27017/mydb', {useNewUrlParser: true, useUn
 
 app.use('/', require('./routes/index'));
 app.use('/public', express.static('public'));
-
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+/*React root*/
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname + '../frontend/build/index.html'));
+});
 app.listen(3001, () => {
   console.log('Server is listening on port 3001');
 });
---
+++ frontend/build/precache-manifest.a3969e77a2f9aeb78dfd2869fdfd4e6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71356389b0db711697fc878c494bafc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5ecc21bced8411d9db",
+    "url": "/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "5933e5e0befaf1d2eeae",
+    "url": "/static/css/main.b86cb5fc.chunk.css"
+  },
+  {
+    "revision": "ed5ecc21bced8411d9db",
+    "url": "/static/js/2.89603a00.chunk.js"
+  },
+  {
+    "revision": "9ba832458c82b1b1f4a5b86ec6911b39",
+    "url": "/static/js/2.89603a00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5933e5e0befaf1d2eeae",
+    "url": "/static/js/main.77354e65.chunk.js"
+  },
+  {
+    "revision": "2470ee7ea77dee49b488",
+    "url": "/static/js/runtime-main.57a8b9e2.js"
+  }
+]);
