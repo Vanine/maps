c016d9dad36ba842f3a00990ddd71e4c4bf3e193
Vanine Ghazaryan <vanine.ghazaryan01@gmail.com>
2020-01-09 12:48:46 +0400

    Added heroku configuration

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Cors = require('cors');
 const path = require('path');
+const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(Cors());
@@ -13,20 +14,21 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 })
-mongoose.connect('mongodb://localhost:27017/mydb', {useNewUrlParser: true, useUnifiedTopology: true}, function(err, database) {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mydb', {useNewUrlParser: true, useUnifiedTopology: true}, function(err, database) {
     if (err) throw err;
     console.log("Connected to Mongo");
     });
 
 app.use('/', require('./routes/index'));
-app.use('/public', express.static('public'));
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-/*React root*/
-app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname + '../frontend/build/index.html'));
-});
-app.listen(3001, () => {
-  console.log('Server is listening on port 3001');
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 
